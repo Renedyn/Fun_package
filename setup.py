0d33855e4d214da67ae178289040315a9aa9cdbc
Torrentov <bekyan.artem@gmail.com>
2019-09-21 11:14:28 +0300

    first commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Petr Srakovorodnikov",
     author_email="petya.obosralsya@mail.ru",
     description="A package to draw dicks",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Torrentov/Fun_package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
